bd1a63c24b55ed913a859f132a31a40c20f53c96
pc <pc@DESKTOP-PJJV610>
2025-07-05 04:13:32 +0300

    Dashboard consume edilmeye başlandı.

---
+++ Frontends/BaristaCafe/Controllers/AboutCafeController.cs
@@ -0,0 +1,41 @@
+﻿using BaristaCafe.Application.Interfaces;
+using BaristaCafe.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaristaCafe.Controllers
+{
+    public class AboutCafeController : Controller
+    {
+        private readonly IRepository<AboutCafe> _repository;
+
+        public AboutCafeController(IRepository<AboutCafe> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IActionResult> AboutCafe()
+        {
+            var aboutCafe = await _repository.GetAllAsync();
+            return View(aboutCafe.FirstOrDefault()); // sadece bir kayıt varsa
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAboutCafe(AboutCafe aboutCafe)
+        {
+            if (aboutCafe == null)
+            {
+                // Model gelmiyorsa
+                return BadRequest("Model boş geldi");
+            }
+
+            Console.WriteLine($"ID: {aboutCafe.AboutCafeId}, Title: {aboutCafe.Title}");
+
+            if (ModelState.IsValid)
+            {
+                await _repository.UpdateAsync(aboutCafe);
+                return RedirectToAction("AboutCafe");
+            }
+            return View("AboutCafe", aboutCafe);
+        }
+    }
+}
---
+++ Frontends/BaristaCafe/Controllers/WelcomeScreenController.cs
@@ -0,0 +1,36 @@
+﻿using BaristaCafe.Application.Interfaces;
+using BaristaCafe.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaristaCafe.Controllers
+{
+    public class WelcomeScreenController : Controller
+    {
+        private readonly IRepository<WelcomeScreen> _repository;
+
+        public WelcomeScreenController(IRepository<WelcomeScreen> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IActionResult> WelcomeScreenDetails()
+        {
+            var welcomeScreen = await _repository.GetAllAsync();
+            return View(welcomeScreen.FirstOrDefault());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateWS(WelcomeScreen model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await _repository.UpdateAsync(model);
+
+            // Güncelledikten sonra yine aynı sayfaya dön
+            return RedirectToAction(nameof(WelcomeScreenDetails));
+        }
+    }
+}
